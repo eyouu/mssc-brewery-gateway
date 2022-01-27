426d616b6255e2cb1ca3d64d341cc6f45261160e
Vasyl <vazhovnir@gmail.com>
2022-01-27 16:43:07 +0200

    updates

--- src/main/java/com/whosaidmeow/gateway/MsscBreweryGatewayApplication.java
+++ src/main/java/com/whosaidmeow/gateway/MsscBreweryGatewayApplication.java
@@ -2,6 +2,9 @@ package com.whosaidmeow.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MsscBreweryGatewayApplication {
@@ -10,4 +13,13 @@ public class MsscBreweryGatewayApplication {
         SpringApplication.run(MsscBreweryGatewayApplication.class, args);
     }
 
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/googlesearch2")
+                        .filters(f -> f.rewritePath("/googlesearch2(?<segment>/?.*)", "/${segment}"))
+                        .uri("https://google.com"))
+                .build();
+    }
 }
