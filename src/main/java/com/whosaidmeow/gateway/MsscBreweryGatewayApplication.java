d8b1662a4520fc561764c7c97f9a0782e3db56b6
Vasyl <vazhovnir@gmail.com>
2022-02-03 15:54:59 +0200

    init

--- src/main/java/com/whosaidmeow/gateway/MsscBreweryGatewayApplication.java
+++ src/main/java/com/whosaidmeow/gateway/MsscBreweryGatewayApplication.java
@@ -36,7 +36,12 @@ public class MsscBreweryGatewayApplication {
                 .route(r -> r.path("/api/v1/customers/**")
                         .uri("lb://beer-order-service"))
                 .route(r -> r.path("/api/v1/beer/*/inventory")
+                        .filters(f -> f.circuitBreaker(cb -> cb.setName("inventoryCB")
+                                .setFallbackUri("forward:/inventory-failover")
+                                .setRouteId("inv-failover")))
                         .uri("lb://beer-inventory-service"))
+                .route(r -> r.path("/inventory-failover/**")
+                        .uri("lb://inventory-failover"))
                 .build();
     }
 }
